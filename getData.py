87ab32bda0fe7fcc4f34fe3b6f74399080d0048e
Artur Herczeg <Artur_Herczeg@epam.com>
2016-01-25 09:36:33 +0100

    getEmployees accepts lambdas

--- getData.py
+++ getData.py
@@ -5,46 +5,47 @@ Created on Tue Jan 19 14:02:36 2016
 @author: Artur_Herczeg
 """
 
+import json
+import os.path
+
+from requests.auth import HTTPBasicAuth
+
 from utilities import loadJsonFile, createDir
 import E3S.Employees.DSL as dsl
 from E3S.Employees.data import EmployeeData
 import E3S.connection as e3s
 import E3S.Employees.stats as stats
 
-from requests.auth import HTTPBasicAuth
-import json
-import os.path
-
 dataFolder = 'data'
 
 def getAuthData():
-    config = loadJsonFile("password.cfg")    
-    
-    user=config["service"]["user"]
-    pas =config["service"]["pass"]
+    config = loadJsonFile("password.cfg")
+
+    user = config["service"]["user"]
+    pas = config["service"]["pass"]
     return (user, pas)
-    
 
 
-def getEmployees(auth, limit):
-    
-    
-    query = e3s.getEmptyQuery()   
-    #query = dsl.filterByTitle(query, "Software Engineer")
-    query = dsl.filterByCountry(query, "Hungary")
-    query = dsl.filterByPrimarySkill(query, ".NET")
-    if(limit is not None):
-        query = e3s.setLimits(query, limit)
-    
+
+def getEmployees(auth, filters):
+
+    def evaluateFilters(query):
+        for f in filters:
+            query = f(query)
+        return query
+
+    query = e3s.getEmptyQuery()
+    evaluateFilters(query)
+
     fullQuery = {
-            'metaType': 'meta:people-suite:people-api:com.epam.e3s.app.people.api.data.EmployeeEntity',
-            'query': json.dumps(query)
-        }
+        'metaType': 'meta:people-suite:people-api:com.epam.e3s.app.people.api.data.EmployeeEntity',
+        'query': json.dumps(query)
+    }
 
     rawDataPath = os.path.join(dataFolder, 'rawdata.json')
-    
-    if(not os.path.exists(rawDataPath)):
-        print("No local data cache (rawdata.json) found, getting data from service...")    
+
+    if not os.path.exists(rawDataPath):
+        print("No local data cache (rawdata.json) found, getting data from service...")
         result = e3s.executeQuery(fullQuery, auth)
         empdata = EmployeeData(data=result)
         empdata.saveAsJson(rawDataPath)
@@ -53,11 +54,11 @@ def getEmployees(auth, limit):
         result = loadJsonFile(rawDataPath)
         empdata = EmployeeData(data=result)
 
-    empdata.saveAsSQlite()    
-    
+    empdata.saveAsSQlite()
+
     print("Total number of employees: %d" % result["total"])
     return empdata.dataFrame
-    
+
 def printStat(df, title, plot=None):
     print("================ %s ================ \n" % title)
     print(df)
@@ -71,8 +72,12 @@ createDir(dataFolder)
 user, pas = getAuthData()
 auth = HTTPBasicAuth(user, pas)
 
-
-data = getEmployees(auth, (0, 200)) 
+filters = [
+    lambda x: dsl.filterByCountry(x, "Hungary"),
+    lambda x: dsl.filterByPrimarySkill(x, "Java"),
+    lambda x: e3s.setLimits(x, (0, 1000))
+]
+data = getEmployees(auth, filters=filters)
 empstat = stats.EmployeeStats(data)
 
 printStat(empstat.getNonbillables(), "Non-billables")
