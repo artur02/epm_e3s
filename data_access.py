82b95686b7f994b23ce54058e180410176d842b0
Artur Herczeg <Artur_Herczeg@epam.com>
2016-01-27 13:34:26 +0100

    Stale cache file management

--- data_access.py
+++ data_access.py
@@ -10,7 +10,11 @@ import os.path
 
 from requests.auth import HTTPBasicAuth
 
-from utilities import load_json_file, create_dir
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
+
+from utilities import load_json_file, create_dir, isFileStale
 import E3S.Employees.DSL as dsl
 from E3S.Employees.data import EmployeeData
 import E3S.connection as e3s
@@ -34,6 +38,13 @@ def get_employees(auth, name, filters):
             query = f(query)
         return query
 
+    def is_cache_available(path):
+        file_exists = os.path.exists(raw_data_path)
+        if file_exists:
+            return not isFileStale(path)
+        else:
+            return False
+
     query = e3s.get_empty_query()
     evaluate_filters(query)
 
@@ -42,10 +53,10 @@ def get_employees(auth, name, filters):
         'query': json.dumps(query)
     }
 
-    raw_data_path = os.path.join(FOLDER_DATA, 'rawdata.json')
+    raw_data_path = os.path.join(FOLDER_DATA, 'rawdata_%s.json' % name)
 
-    if not os.path.exists(raw_data_path):
-        print("No local data cache (rawdata.json) found, getting data from service...")
+    if not is_cache_available(raw_data_path):
+        print("No local data cache (%s) found (or outdated), getting data from service..." % raw_data_path)
         result = e3s.execute_query(query_full, auth)
         empdata = EmployeeData(data=result)
         empdata.save_as_json(raw_data_path)
@@ -59,31 +70,59 @@ def get_employees(auth, name, filters):
     print("Total number of employees: %d" % result["total"])
     return empdata.data_frame
 
-def print_stat(df, title, plot=None):
+def print_stat(df, title, save=True, plot_kind=None, plot_name=None):
     print("================ %s ================ \n" % title)
     print(df)
     print("--- Count: %d" % len(df))
-    if plot is not None:
-        df.plot(kind=plot)
+    if plot_kind is not None:
+        fig = plt.figure()
+        df.plot(kind=plot_kind, title=title)
+        if plot_name is not None:
+            figurepath = os.path.join(FOLDER_DATA, '%s_%s.png' % (plot_name, title))
+
+            plt.gcf().subplots_adjust(left=0.35)
+            fig.savefig(figurepath, dpi=300)
+    if save:
+        pass
     print("\n")
 
 def process_java_hungary_employees(auth):
+    NAME = 'JavaHungary'
+
     java_hungary_filters = [
         lambda x: dsl.filter_by_country(x, "Hungary"),
         lambda x: dsl.filter_by_primaryskill(x, "Java"),
         lambda x: e3s.set_limits(x, (0, 1000))
     ]
-    data = get_employees(auth, name='JavaHungary', filters=java_hungary_filters)
+    data = get_employees(auth, name=NAME, filters=java_hungary_filters)
     empstat = stats.EmployeeStats(data)
 
     print_stat(empstat.get_nonbillables(), "Non-billables")
     print_stat(empstat.get_RMs(), "RMs")
     print_stat(empstat.get_cities(), "Cities")
-    print_stat(empstat.get_title_count(), "Titles", plot='barh')
+    print_stat(empstat.get_title_count(), "Titles", plot_kind='barh', plot_name=NAME)
+
+    return (NAME, data)
+
+def process_dotnet_hungary_employees(auth):
+    NAME = 'DotNetHungary'
+
+    dotnet_hungary_filters = [
+        lambda x: dsl.filter_by_country(x, "Hungary"),
+        lambda x: dsl.filter_by_primaryskill(x, ".NET"),
+        lambda x: e3s.set_limits(x, (0, 1000))
+    ]
+    data = get_employees(auth, name=NAME, filters=dotnet_hungary_filters)
+    empstat = stats.EmployeeStats(data)
+
+    print_stat(empstat.get_nonbillables(), "Non-billables")
+    print_stat(empstat.get_RMs(), "RMs")
+    print_stat(empstat.get_cities(), "Cities")
+    print_stat(empstat.get_title_count(), "Titles", plot_kind='barh', plot_name=NAME)
+
+    return (NAME, data)
 
 create_dir(FOLDER_DATA)
 
 USER, PASSWORD = get_auth_data()
 AUTH = HTTPBasicAuth(USER, PASSWORD)
-
-process_java_hungary_employees(AUTH)
--- utilities.py
+++ utilities.py
@@ -8,6 +8,8 @@ Created on Fri Jan 22 10:42:23 2016
 import json
 import os
 
+from datetime import date, timedelta
+
 def load_json_file(fileName):
     f = open(fileName)
     return json.load(f)
@@ -15,3 +17,9 @@ def load_json_file(fileName):
 def create_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+        
+def isFileStale(path, validity = timedelta(days=1)):
+    mod_date = date.fromtimestamp(os.path.getmtime(path))
+    curr_date = date.today()
+    delta = curr_date - mod_date
+    return delta >= validity
